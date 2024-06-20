ae4ecef101751aeabb7d64969774008bb1ae4ea7
Ivan Bongiovanni <bongiovanniivan12gmail.com>
2024-06-20 18:29:49 -0300

    chore: more changes

--- app/(routes)/components/sidebar-search.tsx
+++ app/(routes)/components/sidebar-search.tsx
@@ -5,9 +5,9 @@ import {Input} from "@/components/ui/input"
 
 export const SidebarSearch = ({}) => {
   return (
-    <aside className="max-w-lg overflow-hidden px-6 py-2">
-      <section className="flex w-full flex-col items-start gap-y-4">
-        {/* buscador */}
+    <section className="w-full px-6 py-2">
+      {/* buscador */}
+      <section className="sticky top-0 flex h-full w-full items-center justify-center bg-black py-3">
         <div className="flex w-full items-center rounded-3xl bg-gray-500/45 px-4">
           <TagIcon className="h-8 w-8" name="explore" />
           <Input
@@ -16,141 +16,141 @@ export const SidebarSearch = ({}) => {
             type="text"
           />
         </div>
-        {/* Cards */}
-        <div className="flex w-full flex-col items-start gap-y-4">
-          <CardSidebarSearch className="w-full border bg-transparent" title="Suscribite a Premiun">
-            <div className="flex flex-col items-start gap-y-4">
-              <p className="text-sm font-semibold">
-                Suscríbete para desbloquear nuevas funciones y, si eres elegible, recibir un pago de
-                cuota de ingresos por anuncios.
-              </p>
-              <Button className="bg-sky-600">Suscribirse</Button>
-            </div>
-          </CardSidebarSearch>
-
-          {/* en directo een X */}
-
-          <CardSidebarSearch className="w-full border bg-transparent" title="En directo en X">
-            <section className="flex flex-col gap-y-4">
-              <article className="flex w-full items-start gap-x-2 overflow-hidden">
-                <div className="max-w-44">
-                  <p className="text-sm">#CafeLibertario 🥐☕🛠️ Hincarle un diente al universo 2</p>
-                </div>
-                <div className="rounded-full border-2 border-violet-600">
-                  <div className="flex items-center justify-between p-2">
-                    <div className="flex h-full overflow-hidden">
-                      <img
-                        alt=""
-                        className="h-6 w-6 rounded-full object-cover"
-                        src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
-                      />
-                      <img
-                        alt=""
-                        className="h-6 w-6 rounded-full object-cover"
-                        src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
-                      />
-                      <img
-                        alt=""
-                        className="h-6 w-6 rounded-full object-cover"
-                        src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
-                      />
-                    </div>
-                    <span className="text-sm">+38</span>
-                  </div>
-                </div>
-              </article>
-              <article className="flex w-full items-start gap-x-2 overflow-hidden">
-                <div className="max-w-44">
-                  <p className="text-sm">Edem @ Sobre La Venus de las pieles porneeta</p>
-                </div>
-                <div className="rounded-full border-2 border-violet-600">
-                  <div className="flex items-center justify-between p-2">
-                    <div className="flex h-full overflow-hidden">
-                      <img
-                        alt=""
-                        className="h-6 w-6 rounded-full object-cover"
-                        src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
-                      />
-                      <img
-                        alt=""
-                        className="h-6 w-6 rounded-full object-cover"
-                        src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
-                      />
-                      <img
-                        alt=""
-                        className="h-6 w-6 rounded-full object-cover"
-                        src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
-                      />
-                    </div>
-                    <span className="text-sm">+38</span>
-                  </div>
-                </div>
-              </article>
-            </section>
-          </CardSidebarSearch>
-
-          {/* A quien seguir */}
-
-          <CardSidebarSearch className="w-full border bg-transparent" title="A quien seguir">
-            <section className="flex w-full flex-col gap-y-4">
-              <article className="flex w-full items-start gap-x-2">
-                <div className="h-12 w-16 overflow-hidden rounded-full">
-                  <img
-                    alt=""
-                    className="aspect-square h-full w-full object-cover"
-                    src="https://pbs.twimg.com/profile_images/1790004218088050689/nhev9ozZ_400x400.jpg"
-                  />
-                </div>
-                <div className="flex w-full items-center justify-between">
-                  <div className="flex flex-col items-start">
-                    <h3 className="text-lg font-bold">La nación Más</h3>
-                    <span className="text-sm font-extralight text-gray-500">@lanacionmas</span>
-                  </div>
-                  <Button className="bg-white text-black" variant="outline">
-                    Seguir
-                  </Button>
-                </div>
-              </article>
-              <article className="flex w-full items-start gap-x-2">
-                <div className="h-12 w-16 overflow-hidden rounded-full">
-                  <img
-                    alt=""
-                    className="aspect-square h-full w-full object-cover"
-                    src="https://pbs.twimg.com/profile_images/1790004218088050689/nhev9ozZ_400x400.jpg"
-                  />
-                </div>
-                <div className="flex w-full items-center justify-between">
-                  <div className="flex flex-col items-start">
-                    <h3 className="text-lg font-bold">La nación Más</h3>
-                    <span className="text-sm font-extralight text-gray-500">@lanacionmas</span>
-                  </div>
-                  <Button className="bg-white text-black" variant="outline">
-                    Seguir
-                  </Button>
-                </div>
-              </article>
-              <article className="flex w-full items-start gap-x-2">
-                <div className="h-12 w-16 overflow-hidden rounded-full">
-                  <img
-                    alt=""
-                    className="aspect-square h-full w-full object-cover"
-                    src="https://pbs.twimg.com/profile_images/1790004218088050689/nhev9ozZ_400x400.jpg"
-                  />
-                </div>
-                <div className="flex w-full items-center justify-between">
-                  <div className="flex flex-col items-start">
-                    <h3 className="text-lg font-bold">La nación Más</h3>
-                    <span className="text-sm font-extralight text-gray-500">@lanacionmas</span>
-                  </div>
-                  <Button className="bg-white text-black" variant="outline">
-                    Seguir
-                  </Button>
-                </div>
-              </article>
-            </section>
-          </CardSidebarSearch>
-        </div>
       </section>
-    </aside>
+
+      <section className="flex w-full flex-col items-start gap-y-4">
+        <CardSidebarSearch className="w-full border bg-transparent" title="Suscribite a Premiun">
+          <div className="flex flex-col items-start gap-y-4">
+            <p className="text-sm font-semibold">
+              Suscríbete para desbloquear nuevas funciones y, si eres elegible, recibir un pago de
+              cuota de ingresos por anuncios.
+            </p>
+            <Button className="bg-sky-600">Suscribirse</Button>
+          </div>
+        </CardSidebarSearch>
+
+        {/* en directo een X */}
+
+        <CardSidebarSearch className="w-full border bg-transparent" title="En directo en X">
+          <section className="flex flex-col gap-y-4">
+            <article className="flex w-full items-start gap-x-2 overflow-hidden">
+              <div className="max-w-44">
+                <p className="text-sm">#CafeLibertario 🥐☕🛠️ Hincarle un diente al universo 2</p>
+              </div>
+              <div className="rounded-full border-2 border-violet-600">
+                <div className="flex items-center justify-between p-2">
+                  <div className="flex h-full overflow-hidden">
+                    <img
+                      alt=""
+                      className="h-6 w-6 rounded-full object-cover"
+                      src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
+                    />
+                    <img
+                      alt=""
+                      className="h-6 w-6 rounded-full object-cover"
+                      src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
+                    />
+                    <img
+                      alt=""
+                      className="h-6 w-6 rounded-full object-cover"
+                      src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
+                    />
+                  </div>
+                  <span className="text-sm">+38</span>
+                </div>
+              </div>
+            </article>
+            <article className="flex w-full items-start gap-x-2 overflow-hidden">
+              <div className="max-w-44">
+                <p className="text-sm">Edem @ Sobre La Venus de las pieles porneeta</p>
+              </div>
+              <div className="rounded-full border-2 border-violet-600">
+                <div className="flex items-center justify-between p-2">
+                  <div className="flex h-full overflow-hidden">
+                    <img
+                      alt=""
+                      className="h-6 w-6 rounded-full object-cover"
+                      src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
+                    />
+                    <img
+                      alt=""
+                      className="h-6 w-6 rounded-full object-cover"
+                      src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
+                    />
+                    <img
+                      alt=""
+                      className="h-6 w-6 rounded-full object-cover"
+                      src="https://i.ytimg.com/vi/8HqE2s5u-6k/maxresdefault.jpg"
+                    />
+                  </div>
+                  <span className="text-sm">+38</span>
+                </div>
+              </div>
+            </article>
+          </section>
+        </CardSidebarSearch>
+
+        {/* A quien seguir */}
+
+        <CardSidebarSearch className="w-full border bg-transparent" title="A quien seguir">
+          <section className="flex w-full flex-col gap-y-4">
+            <article className="flex w-full items-start gap-x-2">
+              <div className="h-12 w-16 overflow-hidden rounded-full">
+                <img
+                  alt=""
+                  className="aspect-square h-full w-full object-cover"
+                  src="https://pbs.twimg.com/profile_images/1790004218088050689/nhev9ozZ_400x400.jpg"
+                />
+              </div>
+              <div className="flex w-full items-center justify-between">
+                <div className="flex flex-col items-start">
+                  <h3 className="text-lg font-bold">La nación Más</h3>
+                  <span className="text-sm font-extralight text-gray-500">@lanacionmas</span>
+                </div>
+                <Button className="bg-white text-black" variant="outline">
+                  Seguir
+                </Button>
+              </div>
+            </article>
+            <article className="flex w-full items-start gap-x-2">
+              <div className="h-12 w-16 overflow-hidden rounded-full">
+                <img
+                  alt=""
+                  className="aspect-square h-full w-full object-cover"
+                  src="https://pbs.twimg.com/profile_images/1790004218088050689/nhev9ozZ_400x400.jpg"
+                />
+              </div>
+              <div className="flex w-full items-center justify-between">
+                <div className="flex flex-col items-start">
+                  <h3 className="text-lg font-bold">La nación Más</h3>
+                  <span className="text-sm font-extralight text-gray-500">@lanacionmas</span>
+                </div>
+                <Button className="bg-white text-black" variant="outline">
+                  Seguir
+                </Button>
+              </div>
+            </article>
+            <article className="flex w-full items-start gap-x-2">
+              <div className="h-12 w-16 overflow-hidden rounded-full">
+                <img
+                  alt=""
+                  className="aspect-square h-full w-full object-cover"
+                  src="https://pbs.twimg.com/profile_images/1790004218088050689/nhev9ozZ_400x400.jpg"
+                />
+              </div>
+              <div className="flex w-full items-center justify-between">
+                <div className="flex flex-col items-start">
+                  <h3 className="text-lg font-bold">La nación Más</h3>
+                  <span className="text-sm font-extralight text-gray-500">@lanacionmas</span>
+                </div>
+                <Button className="bg-white text-black" variant="outline">
+                  Seguir
+                </Button>
+              </div>
+            </article>
+          </section>
+        </CardSidebarSearch>
+      </section>
+    </section>
   )
 }
--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -96,27 +96,29 @@ export default async function HomePageX() {
 
   return (
     <section className="col-span-5 border-r">
-      <header className="group relative flex items-center overflow-hidden border-b">
-        <div className="flex">
-          {headerOptions.map((option) => (
-            <Link key={option} href={`/${option}`}>
-              <Button
-                className="rounded-none p-5 text-base font-extralight"
-                size="sm"
-                variant="ghost"
-              >
-                {option}
-              </Button>
-            </Link>
-          ))}
-        </div>
-        <Button
-          className="absolute right-0 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-          variant="ghost"
-        >
-          ➡️
-        </Button>
-      </header>
+      <section className="sticky top-0 border-b bg-black/10 py-1 backdrop-blur-xl">
+        <header className="group relative flex items-center overflow-hidden">
+          <div className="flex">
+            {headerOptions.map((option) => (
+              <Link key={option} href={`/${option}`}>
+                <Button
+                  className="rounded-none p-5 text-base font-extralight"
+                  size="sm"
+                  variant="ghost"
+                >
+                  {option}
+                </Button>
+              </Link>
+            ))}
+          </div>
+          <Button
+            className="absolute right-0 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+            variant="ghost"
+          >
+            ➡️
+          </Button>
+        </header>
+      </section>
       {/* form post a tweet */}
       <div className="mt-2">
         <TweetForm currentUser={currentUser} />
